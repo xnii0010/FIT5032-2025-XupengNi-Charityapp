e37650ac36d3c4a0c379efa2f15d7ded9e95ac84
xnii0010 <xnii0010@student.monash.edu>
2025-07-21 20:15:17 +0800

    Set the basic standards of home page.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import './assets/main.css'
 
 const app = createApp(App)
 
